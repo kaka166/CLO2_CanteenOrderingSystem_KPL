6b2e5349beba8525e9c52e9f6bd1107d47de2e17
DuckyDash <andikasutarni@gmail.com>
2025-04-29 09:13:15 +0700

    update utilities.js

---
+++ utils.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')();
+
+function GenerateOrderId() {
+    return Math.floor(100000 + Math.random() * 9000000);
+}
+
+function GenerateOrderDate() {
+    const date = new Date();
+    return date.toISOString().replace('T', ' ').split('.')[0];
+}
+
+function input (message)
+ {
+    return prompt(message);
+ }
+
+Medule.exports = {
+    GenerateOrderId,
+    GenerateOrderDate,
+    input
+};
