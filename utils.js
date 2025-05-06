d8ebcf075d3168d5cd7f56538aab6b198ddf9a74
DuckyDash <andikasutarni@gmail.com>
2025-05-06 11:39:40 +0700

    fixing clock format

--- utils.js
+++ utils.js
@@ -6,7 +6,16 @@ function generateOrderId() {
 
 function generateOrderDate() {
     const date = new Date();
-    return date.toISOString().replace('T', ' ').split('.')[0];
+    date.setHours(date.getHours());
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
 
 function input (message){
